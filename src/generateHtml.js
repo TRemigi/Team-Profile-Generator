836ee2a41101c2ab1fa88984de25e722ccd2b69a
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-07-05 15:18:53 -0600

    added html template object

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -1,25 +1,49 @@
-// const Team = require('../lib/Team');
+const Html = require('./Html');
+
 
 generateHtml = (team) => {
     console.log(team);
+    let html = new Html();
+
     team.forEach(element => {
         element.role = element.getRole();
         const { name, role, id, email, roleInfo } = element;
         let roleInfoType;
-
         if (element.role === 'Manager') {
-            roleInfoType = 'Office Number:';
+            roleInfoType = 'Office Number';
         } else if (element.role === 'Engineer') {
-            roleInfoType = 'GitHub Username:'; 
+            roleInfoType = 'GitHub Username'; 
         } else {
-            roleInfoType = 'School:';
+            roleInfoType = 'School';
         }
-        console.log(`
-        ${name} is an ${role}.
-        Id: ${id}
-        Email Address: ${email}
-        ${roleInfoType} ${roleInfo}`)
+
+        html.markDown = 
+        `${html.markDown}
+        <div class="card">
+            <div class="card-header text-white bg-dark mb-3">
+                <h2>${name}</h2>
+                <h4>${role}<span>icon</span></h4>
+            </div>
+                <div class="card-body font-weight-bold">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${id}</li>
+                        <li class="list-group-item">Email:
+                            <a href="${email}">${email}</a>
+                        </li>
+                        <li class="list-group-item">${roleInfoType}:
+                            <a href="${roleInfo}">${roleInfo}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>`
     });
+
+    html.markDown = 
+    `${html.markDown}
+    ${html.markDownFooter}`;
+
+    console.log(html.markDown);
 }
 
 module.exports = generateHtml;
